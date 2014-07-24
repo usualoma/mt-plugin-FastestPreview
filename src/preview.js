9a3b3552c4735e29af25e4012801e8eb6bbedf2a
Taku AMANO <taku@taaas.jp>
2014-07-25 06:22:18 +0900

    Check carefully.

--- src/preview.js
+++ src/preview.js
@@ -28,7 +28,7 @@
     }
 
     window.addEventListener("message", function(event) {
-        if (! event.data.values.length) {
+        if (! event.data.values || ! event.data.values.length) {
             return;
         }
 
